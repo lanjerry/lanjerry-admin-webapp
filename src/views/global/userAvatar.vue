75d7fa216d97ec944f8ce3980f0e83ae603e52b4
lanjerry <linjierong@mayiic.com>
2020-01-03 16:50:08 +0800

    优化代码

--- src/views/global/userAvatar.vue
+++ src/views/global/userAvatar.vue
@@ -24,7 +24,7 @@
       <br />
       <el-row>
         <el-col :lg="2" :md="2">
-          <el-upload :show-file-list="false" :before-upload="beforeUpload">
+          <el-upload action="#" :http-request="requestUpload" :show-file-list="false" :before-upload="beforeUpload">
             <el-button size="small">
               上传
               <i class="el-icon-upload el-icon--right"></i>
@@ -44,7 +44,7 @@
           <el-button icon="el-icon-refresh-right" size="small" @click="rotateRight()"></el-button>
         </el-col>
         <el-col :lg="{span: 2, offset: 6}" :md="2">
-          <el-button type="primary" size="small" @click="uploadImg()">上 传</el-button>
+          <el-button type="primary" size="small" @click="uploadImg()">提 交</el-button>
         </el-col>
       </el-row>
     </el-dialog>
@@ -52,84 +52,87 @@
 </template>
 
 <script>
-import store from "@/store";
-import { VueCropper } from "vue-cropper";
-import { uploadAvatar } from "@/api/global/currentUser";
+  import store from "@/store";
+  import { VueCropper } from "vue-cropper";
+  import { uploadAvatar } from "@/api/global/currentUser";
 
-export default {
-  components: { VueCropper },
-  props: {
-    user: {
-      type: Object
-    }
-  },
-  data() {
-    return {
-      // 是否显示弹出层
-      open: false,
-      // 弹出层标题
-      title: "修改头像",
-      options: {
-        img: store.getters.avatar, //裁剪图片的地址
-        autoCrop: true, // 是否默认生成截图框
-        autoCropWidth: 200, // 默认生成截图框宽度
-        autoCropHeight: 200, // 默认生成截图框高度
-        fixedBox: true // 固定截图框大小 不允许改变
-      },
-      previews: {}
-    };
-  },
-  methods: {
-    // 编辑头像
-    editCropper() {
-      this.open = true;
-    },
-    // 向左旋转
-    rotateLeft() {
-      this.$refs.cropper.rotateLeft();
-    },
-    // 向右旋转
-    rotateRight() {
-      this.$refs.cropper.rotateRight();
-    },
-    // 图片缩放
-    changeScale(num) {
-      num = num || 1;
-      this.$refs.cropper.changeScale(num);
-    },
-    // 上传预处理
-    beforeUpload(file) {
-      if (file.type.indexOf("image/") == -1) {
-        this.msgError("文件格式错误，请上传图片类型,如：JPG，PNG后缀的文件。");
-      } else {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = () => {
-          this.options.img = reader.result;
-        };
+  export default {
+    components: { VueCropper },
+    props: {
+      user: {
+        type: Object
       }
     },
-    // 上传图片
-    uploadImg() {
-      this.$refs.cropper.getCropBlob(data => {
-        let formData = new FormData();
-        formData.append("avatarfile", data);
-        uploadAvatar(formData).then(response => {
-          if (response.code === 200) {
-            this.open = false;
-            this.options.img = process.env.VUE_APP_BASE_API + response.imgUrl;
-            this.msgSuccess("修改成功");
-          } else {
-            this.msgError(response.msg);
-          }
-          this.$refs.cropper.clearCrop();
-        });
-      });
+    data() {
+      return {
+        // 是否显示弹出层
+        open: false,
+        // 弹出层标题
+        title: "修改头像",
+        options: {
+          img: store.getters.avatar, //裁剪图片的地址
+          autoCrop: true, // 是否默认生成截图框
+          autoCropWidth: 200, // 默认生成截图框宽度
+          autoCropHeight: 200, // 默认生成截图框高度
+          fixedBox: true // 固定截图框大小 不允许改变
+        },
+        previews: {}
+      };
     },
-    // 实时预览
-    realTime(data) {
-      this.previews = data;
+    methods: {
+      // 编辑头像
+      editCropper() {
+        this.open = true;
+      },
+      // 覆盖默认的上传行为
+      requestUpload() {
+      },
+      // 向左旋转
+      rotateLeft() {
+        this.$refs.cropper.rotateLeft();
+      },
+      // 向右旋转
+      rotateRight() {
+        this.$refs.cropper.rotateRight();
+      },
+      // 图片缩放
+      changeScale(num) {
+        num = num || 1;
+        this.$refs.cropper.changeScale(num);
+      },
+      // 上传预处理
+      beforeUpload(file) {
+        if (file.type.indexOf("image/") == -1) {
+          this.msgError("文件格式错误，请上传图片类型,如：JPG，PNG后缀的文件。");
+        } else {
+          const reader = new FileReader();
+          reader.readAsDataURL(file);
+          reader.onload = () => {
+            this.options.img = reader.result;
+          };
+        }
+      },
+      // 上传图片
+      uploadImg() {
+        this.$refs.cropper.getCropBlob(data => {
+          let formData = new FormData();
+          formData.append("avatarfile", data);
+          uploadAvatar(formData).then(response => {
+            if (response.code === 200) {
+              this.open = false;
+              this.options.img = process.env.VUE_APP_BASE_API + response.imgUrl;
+              this.msgSuccess("修改成功");
+            } else {
+              this.msgError(response.msg);
+            }
+            this.$refs.cropper.clearCrop();
+          });
+        });
+      },
+      // 实时预览
+      realTime(data) {
+        this.previews = data;
+      }
     }
-  }
-};
+  };
 </script>
