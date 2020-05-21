c123f7193498771206475fcc9f1ea50f060fa161
lanjerry <linjierong@mayiic.com>
2020-05-21 11:29:24 +0800

    feat: 新增自定义返回码消息

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import { Message, MessageBox, Notification } from 'element-ui'
 import store from '@/store'
 import { getToken } from '@/utils/auth'
+import errorCode from '@/utils/errorCode'
 
 axios.defaults.headers['Content-Type'] = 'application/json;charset=utf-8'
 // 创建axios实例
@@ -12,29 +13,27 @@ const service = axios.create({
   timeout: 10000
 })
 // request拦截器
-service.interceptors.request.use(
-  config => {
-    if (getToken()) {
-      config.headers['Authorization'] = getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
-    }
-    return config
-  },
-  error => {
-    console.log(error)
-    Promise.reject(error)
+service.interceptors.request.use(config => {
+  // 是否需要设置 token
+  const isToken = (config.headers || {}).isToken === false
+  if (getToken() && !isToken) {
+    config.headers['Authorization'] = getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
   }
-)
+  return config
+}, error => {
+  console.log(error)
+  Promise.reject(error)
+})
 
 // 响应拦截器
 service.interceptors.response.use(res => {
-    const { code, msg } = res.data
-
-    // 200请求成功，203登录异常
-    if (code === 200 || !code) {
-      return res.data
-    } else if (code === 203) {
+    // 未设置状态码则默认成功状态
+    const code = res.data.code || 200;
+    // 获取错误信息
+    const message = res.data.msg || errorCode[code] || errorCode['default']
+    if (code === 203) {
       MessageBox.confirm(
-        '登录状态已过期或者身份异常，请重新登录',
+        '登录状态已过期或者身份异常，您可以继续留在该页面，或者重新登录',
         '系统提示',
         {
           confirmButtonText: '重新登录',
@@ -46,12 +45,19 @@ service.interceptors.response.use(res => {
           location.reload() // 为了重新实例化vue-router对象 避免bug
         })
       })
-    } else {
+    } else if (code === 500) {
+      Message({
+        message: message,
+        type: 'error'
+      })
+      return Promise.reject(new Error(message))
+    } else if (code !== 200) {
       Notification.error({
-        title: '错误',
-        message: msg
+        title: message
       })
       return Promise.reject('error')
+    } else {
+      return res.data
     }
   },
   error => {
