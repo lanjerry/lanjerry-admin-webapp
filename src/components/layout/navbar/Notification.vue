781cbf7139b00799ea871642a7ba19a039480bac
lanjerry <linjierong@mayiic.com>
2020-08-28 10:57:57 +0800

    feat: 消息推送新增心跳机制

--- src/components/layout/navbar/Notification.vue
+++ src/components/layout/navbar/Notification.vue
@@ -14,54 +14,78 @@
     name: 'Notification',
     data() {
       return {
-        badge: 0,
-        websocket: null,
-        lockReconnect: false,
-        wsCreateHandler: null
+        badge: 0, // 通知个数
+        websocket: null, // 建立连接
+        lockReconnect: false, // 是否真正建立连接
+        wsCreateHandler: null, // 重新连接
+        timeout: 5000, // 心跳倒计时
+        timeoutObj: null, // 心跳连接
+        serverTimeOutObj: null // 心跳内部倒计时
       }
     },
     methods: {
+      // 初始化通知个数
       initCount() {
         getUserNotificationCount().then(response => {
           this.badge = response.data
         })
       },
-      createWebSocket() {
+      // 创建消息通知的websocket
+      initWebSocket() {
         try {
-          this.websocket = new WebSocket('ws://127.0.0.1:1000/ws/notification/' + store.getters.id)
-          this.initWsEventHandle()
-        } catch (e) {
-          console.log('webstocket连接出现异常')
-          this.reconnect()
-        }
-      },
-      initWsEventHandle() {
-        try {
+          const wsuri = 'ws://127.0.0.1:1000/ws/notification/' + store.getters.id
+          this.websocket = new WebSocket(wsuri)
           this.websocket.onopen = this.onWsOpen
           this.websocket.onmessage = this.onWsMessage
           this.websocket.onclose = this.onWsClose
           this.websocket.onerror = this.onWsError
         } catch (e) {
-          console.log('绑定事件没有成功')
+          console.log('webstocket连接出现异常')
           this.reconnect()
         }
       },
+      // websocket心跳机制
+      start() {
+        const self = this
+        self.timeoutObj && clearTimeout(self.timeoutObj)
+        self.serverTimeoutObj && clearTimeout(self.serverTimeoutObj)
+        self.timeoutObj = setTimeout(function() {
+          console.log('发送ping到后台')
+          self.websocket.send('ping')
+          // 内嵌定时器
+          self.serverTimeoutObj = setTimeout(() => {
+            console.log('没有收到后台的数据，关闭连接')
+            self.reconnect()
+          }, self.timeout)
+        }, self.timeout)
+      },
+      // 重置心跳
+      reset() {
+        const that = this
+        //清除时间
+        clearTimeout(that.timeoutObj)
+        clearTimeout(that.serverTimeOutObj)
+        //重启心跳
+        that.start()
+      },
+      // websocket断线重连机制
       reconnect() {
-        if (this.lockReconnect) {
+        const that = this
+        if (that.lockReconnect) {
           return
         }
-        this.lockReconnect = true
-        this.wsCreateHandler && clearTimeout(this.wsCreateHandler)
-        const that = this;
-        this.wsCreateHandler = setTimeout(function() {
+        that.lockReconnect = true
+        that.wsCreateHandler && clearTimeout(that.wsCreateHandler)
+        that.wsCreateHandler = setTimeout(() => {
           console.log('开始重连')
-          that.createWebSocket()
+          that.initWebSocket()
           that.lockReconnect = false
           console.log('重连完成')
-        }, 1000)
+        }, 5000)
       },
       onWsOpen() {
         console.log('成功连接webstocket')
+        this.start()
       },
       onWsMessage(e) {
         let json = JSON.parse(e.data)
@@ -76,25 +100,25 @@
         if (json.type == 'NUMBER') {
           this.badge = json.message
         }
+        this.reset()
       },
       onWsClose() {
         console.log('关闭webstocket')
-        this.reconnect()
       },
       onWsError() {
-        this.reconnect()
         this.$notify.error({
           title: '错误',
           message: 'webstocket出现异常'
         })
+        this.reconnect()
       }
     },
-    beforeDestroy: function() {
-      this.onWsClose()
+    created() {
+      this.initCount() // 初始化通知个数
+      this.initWebSocket() // 页面刚进入时开启长连接
     },
-    mounted() {
-      this.initCount()
-      this.createWebSocket()
+    destroyed: function() {
+      this.onWsClose() // 页面销毁时关闭长连接
     }
   }
 </script>
